2fea4da94fb1c53b0d46a29d9d1936c6b12a4c99
Nikolas Novak <nklasakk@gmail.com>
2024-04-28 18:05:35 +0200

    changed parsed interface and added replacing for external address

--- scripts/parseIP.sh
+++ scripts/parseIP.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 ip_output="$(ip a)"
-docker_eth_ip=$(echo "$ip_output" | grep -A 3 "eth1" | awk '/inet / {print $2}')
+docker_eth_ip=$(echo "$ip_output" | grep -A 3 "eth2" | awk '/inet / {print $2}')
 sed -i "s#docker_ip#$docker_eth_ip#g" /etc/asterisk/pjsip.conf
+sed -i "s#external_ip#$MANAGER_IP#g" /etc/asterisk/pjsip.conf
